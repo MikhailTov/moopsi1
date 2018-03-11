6847a4965f3e00399bc22105730e161cac4e5558
andreirumiantsau <andreirumiantsau19842004@gmail.com>
2018-03-11 17:58:13 +0300

    change_3

--- mail_handler.php
+++ mail_handler.php
@@ -10,11 +10,11 @@
     
     $retval = mail($to,$subject,$full_message);
     
-    echo $first_name;
-    echo $last_name;
-    echo $email;
-    echo $subject;
-    echo $message;
+    echo $first_name . "\n";
+    echo $last_name . "\n";
+    echo $email . "\n";
+    echo $subject . "\n";
+    echo $message . "\n";
 
     if ($retval == true) {
         echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
